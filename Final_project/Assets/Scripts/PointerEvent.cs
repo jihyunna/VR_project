49d395e52afcd270660393c3728436286d5890f7
jihyunna <skwlgus106@naver.com>
2022-12-09 19:57:45 +0900

    화재_집
    화재_집

---
+++ Final_project/Assets/Scripts/PointerEvent.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Collider))]
+
+public class PointerEvent : MonoBehaviour
+{
+    public Image LoadingBar;
+    private bool IsOn;
+    private float barTime = 0.0f;
+
+    void Start()
+    {
+        IsOn = false;
+        LoadingBar.fillAmount = 0;
+    }
+
+    void Update()
+    {
+        if (IsOn)
+        {
+            if (barTime <= 5.0f)
+            {
+                barTime += Time.deltaTime;
+            }
+            LoadingBar.fillAmount = barTime / 5.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    public void SetGazedAt(bool gazedAt)
+    {
+        IsOn = gazedAt;
+        barTime = 0.0f;
+        if (gazedAt)
+        {
+            Debug.Log("In");
+        }
+        else
+        {
+            Debug.Log("Out");
+            LoadingBar.fillAmount = 0;
+        }
+    }
+
+}
+
